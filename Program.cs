e578fc82d5e50e9090d7099e390b33b439123e5c
xJaesaa <103669129@student.swin.edu.au>
2021-03-11 16:34:12 +1100

    thing?

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JaesaCodePog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age");
+            string ageStr = Console.ReadLine();
+
+            int age = int.Parse(ageStr);
+
+            if(age >= 16){
+                Console.WriteLine("You may Enter");
+
+                Console.WriteLine("Please enter your test result out of 100");
+                string resultStr = Console.ReadLine();
+
+                int result = int.Parse(resultStr);
+
+                if(result >=80){
+                    Console.WriteLine("enjoy ur license bruv happy trails");
+                }
+                else{
+                    Console.WriteLine("sorry bruv ur too bad ta be on tha road");
+                }
+            }
+            else{
+                Console.WriteLine("too young, begone child");
+            }
+            
+        }
+    }
+}
